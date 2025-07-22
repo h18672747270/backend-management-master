b203f447687c4ca1fb9468b439470d5dfce04b0a
hanle <h18672747270@163.com>
2025-07-22 14:30:36 +0800

    初始化项目

---
+++ vite.config.ts
@@ -0,0 +1,55 @@
+import { defineConfig ,loadEnv} from 'vite';	
+import vue from '@vitejs/plugin-vue';
+import VueSetupExtend from 'vite-plugin-vue-setup-extend';
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
+
+export default defineConfig(({ command, mode }) => {
+  // 获取各种环境下的对应的变量
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    base: './',
+    build: {
+      outDir: mode === 'development' ? '../../development' : '../../production', // 根据环境变量决定输出目录
+    },
+    plugins: [
+      vue(),
+      VueSetupExtend(),
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
+      }),
+    ],
+    optimizeDeps: {
+      include: ['schart.js'],
+    },
+    resolve: {
+      alias: {
+        '@': '/src',
+        '~': '/src/assets',
+      },
+    },
+    define: {
+      __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: "true",
+    },
+    //代理跨域
+    server: {
+      host: true, // 允许外部访问
+      port: 3000, // 可选：指定端口号
+		proxy: {
+		  [env.VITE_APP_BASE_API]: {
+			//获取数据的服务器地址设置
+			target: env.VITE_SERVE,
+			//需要代理跨域
+			changeOrigin: true,
+			//路径重写
+			rewrite: (path) => path.replace(/^\/api/, ''),
+		  }
+		}
+	  }
+  };
+});
